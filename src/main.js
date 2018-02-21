ee6751f442adb4117ce06feec9bb9a831b06fffe
PavelNET <pavlo.netrebsky@gmail.com>
2018-02-21 20:55:14 +0200

    Script, styles

--- src/main.js
+++ src/main.js
@@ -1,15 +1,4 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-    let el = document.getElementsByClassName('option-container');
-    
-    Array.from(el).forEach((item) => {
-        let _self = this;
-        item.addEventListener('click', (e) => {
-            console.log(e)
-        })
-    })
-})
-
 function addClassToElement(elem) {
     elem.classList.toggle('checked');
 }
